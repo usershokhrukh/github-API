c1360aa858eeac8e9d3901ab172bf5cd91a89cdb
usershokhrukh <asxurovsit@gmail.com>
2026-01-29 20:53:36 +0500

    features: github api design changed and upgraded

--- loyiham-nomi/index.js
+++ loyiham-nomi/index.js
@@ -10,44 +10,114 @@ async function getRequest(url) {
 }
 
 try {
+  const elForm = document.querySelector(".main__form");
   function getResponse(url) {
+    showLoad(1);
+    showWarning(0);
+    const result = document.querySelector(".main__result-profile");
+    result.innerHTML = "";
     const responseData = getRequest(url)
       .then((response) => {
         workData(response);
       })
       .catch((error) => {
+        showLoad(0);
+        showWarning(1);
+        elForm.reset();
         throw new Error(error);
       });
   }
-
-  function workData(response) {
-    const result = document.querySelector(".result");
-    const {avatar_url, blog, company, created_at, email, followers, following, id, location, login, public_gists, public_repos, updated_at} = response.data;
-    result.innerHTML = `
-       <img width="70px" height="70px" src="${avatar_url}" alt="img" />
-      <p>blog: <a href="${blog}">${blog}</a></p>
-      <p>company: ${company}</p>
-      <p>created at: ${created_at}</p>
-      <p>email: ${email} </p>
-      <p>followers: ${followers}</p>
-      <p>following:  ${following}</p>
-      <p>id: ${id} </p>
-      <p>location: ${location} </p>
-      <p>login: ${login} </p>
-      <p>public gists: ${public_gists} </p>
-      <p>public repos: ${public_repos} </p>
-      <p>Updated at: ${updated_at}</p>
-    
-    `;
+  function showLoad(index) {
+    const load = document.querySelector(".main__loader");
+    if (index) {
+      load.classList.remove("none");
+      load.classList.add("load");
+    } else {
+      load.classList.add("none");
+      load.classList.remove("load");
+    }
   }
 
-  const elForm = document.querySelector(".form");
-  elForm.addEventListener("submit", (e)=>{
+  function showWarning(index, text) {
+    const warning = document.querySelector(".main__warning");
+    if (index) {
+      warning.classList.remove("none");
+    } else {
+      warning.classList.add("none");
+    }
+  }
+
+  function workData(response) {
+    showLoad(0);
+    const result = document.querySelector(".main__result-profile");
+    const {
+      avatar_url,
+      blog,
+      company,
+      created_at,
+      email,
+      followers,
+      following,
+      id,
+      location,
+      login,
+      public_gists,
+      public_repos,
+      updated_at,
+    } = response.data;
+    result.innerHTML = `
+      <img
+            class="main__result-img"
+            src="${avatar_url}"
+            alt="profile-img"
+          />
+          <div class="main__result-pr-r">
+            <div class="main__result-pr-b">
+              <p class="main__result-pr-rt">
+                company: <span class="main__result-rst">${company}</span>
+              </p>
+              <p class="main__result-pr-rt">
+                created at:
+                <span class="main__result-rst">${created_at}</span>
+              </p>
+              <p class="main__result-pr-rt">
+                email: <span class="main__result-rst">${email}</span>
+              </p>
+            </div>
+
+            <div class="main__result-pr-b">
+              <p class="main__result-pr-rt">
+                followers: <span class="main__result-rst">${followers}</span>
+              </p>
+              <p class="main__result-pr-rt">
+                following: <span class="main__result-rst">${following}</span>
+              </p>
+            </div>
+            <div class="main__result-pr-b">
+              <p class="main__result-pr-rt">
+                location: <span class="main__result-rst">${location}</span>
+              </p>
+              <p class="main__result-pr-rt">
+                repos: <span class="main__result-rst">${public_repos}</span>
+              </p>
+            </div>
+            <button class="main__result-view">view more</button>
+          </div>
+    `;
+    const elMainView = document.querySelector(".main__result-view");
+    elMainView.addEventListener("click", (e) => {
+      window.open(`${response?.data?.html_url}`, "_blank");
+    });
+  }
+
+  elForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const inputValue= elForm["form-input"].value.trim().toLowerCase();
-    const api = `https://api.github.com/users/${inputValue}`
-    getResponse(api);
-  })
+    const inputValue = elForm["form-input"].value.trim().toLowerCase();
+    if (inputValue) {
+      const api = `https://api.github.com/users/${inputValue}`;
+      getResponse(api);
+    }
+  });
 } catch (error) {
   throw new Error(error);
 }
